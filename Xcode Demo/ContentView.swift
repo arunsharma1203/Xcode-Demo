3cf575a65e656dee8336c26d47e9d0d7881d634f
ArexTechnologies <williamblack142@gmail.com>
2024-06-11 09:13:59 +0530

    Initial Commit

---
+++ Xcode Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Xcode Demo
+//
+//  Created by Arun Sharma on 11/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Xcode Demo/Xcode_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_DemoApp.swift
+//  Xcode Demo
+//
+//  Created by Arun Sharma on 11/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
