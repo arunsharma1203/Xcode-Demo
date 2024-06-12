f823b3cfa7eab65d55efe7198d7d8e45141097cb
ArexTechnologies <williamblack142@gmail.com>
2024-06-12 17:56:18 +0530

    This is the first demo with slider and brightness controll

--- Xcode Demo/ContentView.swift
+++ Xcode Demo/ContentView.swift
@@ -1,19 +1,44 @@
-//
-//  ContentView.swift
-//  Xcode Demo
-//
-//  Created by Arun Sharma on 11/06/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State var x = 0.0
+    @State private var isEditing = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("kota")
+                .resizable()
+                .cornerRadius(30.0)
+                .padding(10)
+                .aspectRatio(contentMode: .fit)
+                .brightness(x / 10.0) // Adjust brightness based on the slider value
+                .imageScale(.small)
+                .foregroundStyle(.clear)
+
+            Slider(
+                value: $x,
+                in: 0...10,
+                onEditingChanged: { editing in
+                    isEditing = editing
+                }
+            )
+            .padding(30)
+
+            Text("Adjust Image Brightness")
+
+            Text("\(x)")
+                .padding(.top, 8.0)
+        }
+
+        HStack {
+            Button(action: { x = max(0, x - 1) }, label: {
+                Text("-")
+            })
+            .padding(.trailing, 30.0)
+
+            Button(action: { x = min(10, x + 1) }, label: {
+                Text("+")
+            })
         }
         .padding()
     }
