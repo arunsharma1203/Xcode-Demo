b315f491500faf508560a4e3a16969b4aa9f793c
ArexTechnologies <williamblack142@gmail.com>
2024-06-14 21:23:52 +0530

    Haptic and centered yellow line

---
+++ Xcode Demo/CustomSliderView..swift
@@ -0,0 +1,8 @@
+//
+//  CustomSliderView..swift
+//  Xcode Demo
+//
+//  Created by Arun Sharma on 13/06/24.
+//
+
+import Foundation
---
+++ Xcode Demo/WhiteLineView.swift
@@ -0,0 +1,8 @@
+//
+//  WhiteLineView.swift
+//  Xcode Demo
+//
+//  Created by Arun Sharma on 12/06/24.
+//
+
+import Foundation
